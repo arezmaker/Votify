435064956c04e15336171e52bfa68dd1b52abeea
Jannat <jannat>
2022-10-13 21:29:03 +0500

    update

---
+++ app/src/main/java/com/example/CandidateLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CandidateLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.candidate_login);
+}
+}
