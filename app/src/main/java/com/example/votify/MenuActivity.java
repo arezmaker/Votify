2b0bd3f57021e9249949e3885beb0b5a7054071c
Arez <arezmaker@gmail.com>
2022-11-23 22:33:39 +0500

    master change

--- app/src/main/java/com/example/votify/MenuActivity.java
+++ app/src/main/java/com/example/votify/MenuActivity.java
@@ -2,11 +2,13 @@ package com.example.votify;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,34 +16,35 @@ public class MenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_menu);
 
     }
-//    public void popup(View v)
-//    {
-//        PopupMenu popupMenu=new PopupMenu(this,v);
-//        popupMenu.setOnMenuItemClickListener(this);
-//        popupMenu.inflate(R.menu.popup_menu);
-//        popupMenu.show();
-//    }
-//    @Override
-//    public boolean onMenuItemClick(MenuItem item)
-//    {
-//        Intent intent=getIntent();
-//        switch (item.getItemId())
-//        {
-//            case R.id.profile:
-//                Intent i=new Intent(this,ProfileActivity.class);
-//                i.putExtra("CNIC1",intent.getStringExtra("CNIC"));
-//                startActivity(i);
-//                return true;
-//            case R.id.logout:
-//                Intent i1=new Intent(this,MainActivity.class);
-//                startActivity(i1);
-//                return true;
-//            default:
-//                return false;
-//
-//        }
-//
-//    }
+    public void popup(View v)
+    {
+        PopupMenu popupMenu=new PopupMenu(this,v);
+        popupMenu.setOnMenuItemClickListener(this);
+        popupMenu.inflate(R.menu.popup_menu);
+        popupMenu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item)
+    {
+        Intent intent=getIntent();
+        switch (item.getItemId())
+        {
+            case R.id.profile:
+                Intent i=new Intent(this,ProfileActivity.class);
+                i.putExtra("CNIC1",intent.getStringExtra("CNIC"));
+                startActivity(i);
+                return true;
+            case R.id.logout:
+                Intent i1=new Intent(this,MainActivity.class);
+                startActivity(i1);
+                return true;
+            default:
+                return false;
+
+        }
+
+    }
 
     public void gotoCastVote(View view){
         Intent intent=getIntent();
